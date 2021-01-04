125852880269e11e2505f93a8b05c834958443d9
unknown <yashme7125@gmail.com>
2021-01-04 12:54:57 +0530

    initial commit

---
+++ for_loops.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class second {
+
+public static void main(String[] args) {
+ 
+ for(int i = 0; i < 10; i++) {
+     
+     System.out.println("Hello World");   
+
+     }
+
+}
+
+}
