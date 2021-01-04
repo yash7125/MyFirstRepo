6c6474194347d36293ba0d51046a41b7bd505f88
unknown <yashme7125@gmail.com>
2021-01-04 13:20:29 +0530

    for_loops.java

--- for_loops.java
+++ for_loops.java
@@ -8,8 +8,10 @@ public static void main(String[] args) {
  for(int i = 0; i < 10; i++) {
      
      System.out.println("Hello World");   
-
+     System.out.println("Yash Kumar");
      }
+
+   System.out.println("what about while loop?");
 
 }
 
